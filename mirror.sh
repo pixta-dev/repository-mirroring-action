a09bd6afcf02d814e51b5fe924cd62bca8f18477
Alessandro Mariotti <alessandro.mariotti@zupit.it>
2022-11-11 14:21:53 +0100

    fix: Fix 'fatal: remote mirror already exists' by removing any existing remotes before adding new

--- mirror.sh
+++ mirror.sh
@@ -4,5 +4,9 @@ set -eu
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
+
+# Cleanup any previously created remotes to make sure adding the mirror never fails
+git remote remove mirror || true
+
 git remote add mirror "$INPUT_TARGET_REPO_URL"
 git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
